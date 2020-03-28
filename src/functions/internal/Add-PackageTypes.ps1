fff9067ff68c9191d26db9bc9cc064d9455be744
Tomáš Bouda <email@tomasbouda.cz>
2020-03-28 22:05:20 +0100

    fixed package load

--- src/functions/internal/Add-PackageTypes.ps1
+++ src/functions/internal/Add-PackageTypes.ps1
@@ -1,11 +1,12 @@
 function Add-PackageTypes {
 	process {
 		try {
-			# Load all package dlls
-			Get-ChildItem $Global:packagesPath | Where-Object { $_.Name -like '*.dll' } | ForEach-Object { Add-Type -Path $_ -ErrorAction Stop }
+			foreach ($path in (Get-ChildItem $Global:packagesPath | Where-Object { $_.Name -like '*.dll' } | Select-Object -ExpandProperty FullName)){
+				Add-Type -Path $path -ErrorAction Stop
+			}
 		}
 		catch {
-			Write-Error ($_.Exception.GetBaseException().LoaderExceptions | Format-Table | Out-String)
+			Write-Error $_.Exception
 		}
 	}
 }
