c61250f3edb89f116d1e89ad5576b3bc185feb50
Tomáš Bouda <email@tomasbouda.cz>
2020-03-29 15:31:37 +0200

    dummy test script

--- src/tests/test.ps1
+++ src/tests/test.ps1
@@ -1,7 +1,31 @@
 Import-Module "$PSScriptRoot\..\PoShLog.psm1" -Force
 
+# Start-Logger -Console
+
+# Write-InfoLog 'Hurrray, my first log message'
+
+# exit
+
+# # Create and start new logger
+# Start-Logger -MinimumLevel Verbose -FilePath 'C:\Data\my_awesome-.log' -FileRollingInterval Day
+
+# Write-InfoLog 'Hurrray, my first log message'
+# exit
+
+# $position = @{ 
+# 	Latitude = 25
+# 	Longitude = 134 
+# }
+# $elapsedMs = 34
+
+# Write-InfoLog 'Processed {@Position} in {Elapsed:000} ms.' -PropertyValues $position, $elapsedMs
+
+
+# Close-Logger
+# exit
+
 # Level switch allows you to switch minimum logging level
-$levelSwitch = Get-LevelSwitch -MinimumLevel Verbose
+$levelSwitch = New-LevelSwitch -MinimumLevel Verbose
 
 $DebugPreference = "Continue"
 $VerbosePreference = "Continue"
@@ -17,6 +41,9 @@ Write-FatalLog "test fatal"
 # Setup new logger
 New-Logger |
 	Set-MinimumLevel -ControlledBy $levelSwitch |
+	# Add-EnrichWithEnvironment |
+	# Add-EnrichWithExceptionDetails |
+	# Add-SinkExceptionless -ApiKey '' |
 	Add-SinkFile -Path "C:\Logs\test-.txt" -RollingInterval Hour -OutputTemplate '{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception} {Properties:j}{NewLine}' |
 	Add-SinkConsole -OutputTemplate "[{EnvironmentUserName}{MachineName} {Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}" -RestrictedToMinimumLevel Verbose | 
 	Start-Logger
@@ -25,13 +52,13 @@ Write-VerboseLog "test verbose"
 Write-DebugLog -MessageTemplate "test debug asd"
 Write-InfoLog "test info"
 Write-WarningLog "test warning"
-Write-ErrorLog -MessageTemplate "test fatal {asd}, {num}, {@levelSwitch}" -PropertyValues "test1", 123, $levelSwitch
+Write-ErrorLog -MessageTemplate "test error {asd}, {num}, {@levelSwitch}" -PropertyValues "test1", 123, $levelSwitch
 
 try {
-	throw [System.Exception]::new('Some random exception')
+	Get-Content -Path 'asd' -ErrorAction Stop
 }
 catch {
-	Write-FatalLog -Exception $_.Exception -MessageTemplate 'Chyba'
+	Write-FatalLog -Exception $_.Exception -MessageTemplate 'Error reading file!'
 }
 
 Close-Logger
