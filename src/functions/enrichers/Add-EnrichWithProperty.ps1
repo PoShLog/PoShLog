a86a1a185c33cfbcd9fafba47f3f747436eb139d
Tomáš Bouda <email@tomasbouda.cz>
2020-06-17 22:24:12 +0200

    v2.1.0 release notes

--- src/functions/enrichers/Add-EnrichWithProperty.ps1
+++ src/functions/enrichers/Add-EnrichWithProperty.ps1
@@ -1,17 +1,38 @@
-function Add-EnrichWithProperty{
+function Add-EnrichWithProperty {
+	<#
+	.SYNOPSIS
+		Enriches log events with custom property.
+	.DESCRIPTION
+		Enriches log events with custom property. For example script name.
+	.PARAMETER LoggerConfig
+		Instance of LoggerConfiguration
+	.PARAMETER Name
+		The name of the property
+	.PARAMETER Value
+		The value of the property
+	.PARAMETER DestructureObjects
+		If true, and the value is a non-primitive, non-array type, then the value will be converted to a structure; otherwise, unknown types will be converted to scalars, which are generally stored as strings.
+	.INPUTS
+		None
+	.OUTPUTS
+		LoggerConfiguration object allowing method chaining
+	.EXAMPLE
+		PS> New-Logger | Add-EnrichWithProperty -Name ScriptName -Value 'Test' | Add-SinkConsole | Start-Logger
+	#>
+
 	[Cmdletbinding()]
 	param(
-		[Parameter(Mandatory=$true, ValueFromPipeline=$true)]
+		[Parameter(Mandatory = $true, ValueFromPipeline = $true)]
 		[Serilog.LoggerConfiguration]$LoggerConfig,
-		[Parameter(Mandatory=$true)]
+		[Parameter(Mandatory = $true)]
 		[string]$Name,
-		[Parameter(Mandatory=$true)]
+		[Parameter(Mandatory = $true)]
 		[string]$Value,
-		[Parameter(Mandatory=$false)]
+		[Parameter(Mandatory = $false)]
 		[bool]$DestructureObjects = $false
 	)
 
-	process{
+	process {
 		$LoggerConfig = [Serilog.Configuration.LoggerEnrichmentConfiguration]::WithProperty($LoggerConfig.Enrich, $Name, $Value, $DestructureObjects)
 
 		$LoggerConfig
