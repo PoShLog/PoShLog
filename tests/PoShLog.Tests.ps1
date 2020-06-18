7d8fda703fa94b0a7cc8995482611e20da58733e
Michael Zimmerli <michi.zimmerli@gmail.com>
2020-06-18 09:41:01 +0200

    Extend pester setup

--- tests/PoShLog.Tests.ps1
+++ tests/PoShLog.Tests.ps1
@@ -1,17 +1,19 @@
 $here = (Split-Path -Parent $MyInvocation.MyCommand.Path)
+$beforeChangeLocation = Get-Location
 
 Describe "PoShLog-main" {
     It "loads Module" {
         {
             # set location should not be needed, check PSScriptRoot in .psm1
             Set-Location "$here\..\src\"
-            Import-Module PoShLog
+            # Import-Module PoShLog --> Doesnt work!!
+            Import-Module "$here\..\src\PoShLog.psm1" -Force
         } | Should -Not -Throw
     }
 }
 
 # Checks for correct style in the module-functions (commentBasedHelp etc)
-Describe "ServerManagement-styleChecks" {
+Describe "PoShLog-styleChecks" {
     BeforeAll {
         $functions = @( Get-ChildItem -Path $PSScriptRoot\..\src\functions\*.ps1 -Recurse )
     }
@@ -32,55 +34,83 @@ Describe "ServerManagement-styleChecks" {
 }
 
 
-Describe "ServerManagement-extended" {
-    BeforeAll {
-        Import-Module "$here\..\src\PoShLog"
-    }
-    It "should create a new logger without errors" {
-        { 
-            New-Logger |
+Describe "PoShLog-extended" {
+    Context "Test all functions for errors" {
+        It "should create a new logger without errors" {
+            { 
+                New-Logger |
                 Set-MinimumLevel -Value Verbose |
+                Add-SinkPowerShell -OutputTemplate '[{Timestamp:HH:mm:ss}] {Message:lj}' |
                 Add-SinkFile -Path "$here\testlog.log" |
                 Start-Logger
-        }  | Should -Not -Throw
+            }  | Should -Not -Throw
+        }
+        It "should write a verbose log without throwing" {
+            { Write-VerboseLog 'Test verbose message' } | Should -Not -Throw
+        }
+        It "should write a debug log without throwing" {
+            { Write-DebugLog 'Test Debug message' } | Should -Not -Throw
+        }
+        It "should write a info log without throwing" {
+            { Write-InfoLog 'Test info message' } | Should -Not -Throw
+        }
+        It "should write a warning log without throwing" {
+            { Write-WarningLog 'Test warning message' } | Should -Not -Throw
+        }
+        It "should write a error log without throwing" {
+            { Write-ErrorLog 'Test error message' } | Should -Not -Throw
+        }
+        It "should write a fatal log without throwing" {
+            { Write-FatalLog 'Test fatal message' } | Should -Not -Throw
+        }
+        It "should display formatted output without throwing" {
+            {
+                $position = @{
+                    Latitude  = 25
+                    Longitude = 134
+                }
+                $elapsedMs = 34
+                Write-InfoLog 'Processed {@Position} in {Elapsed:000} ms.' -PropertyValues $position, $elapsedMs
+            } | Should -Not -Throw
+        }
+        
+        It "should close logger without throwing" {
+            { Close-Logger } | Should -Not -Throw
+        }
+        AfterAll {
+            Remove-Item "$here\testlog.log"
+        }
     }
 
-    It "should write a verbose log without throwing"{
-        { Write-VerboseLog 'Test verbose message' } | Should -Not -Throw
+    Context "Test console output functionality" {
+        BeforeAll { 
+            New-Logger |
+            Set-MinimumLevel -Value Verbose |
+            Add-SinkPowerShell -OutputTemplate '[{Timestamp:HH:mm:ss}] {Message:lj}' |
+            Start-Logger
+        }
+        It "should write a verbose log" {
+            Write-VerboseLog 'Test verbose message'
+        }
+        It "should write a debug log" {
+            Write-DebugLog 'Test Debug message'
+        }
+        It "should write a info log" {
+            Write-InfoLog 'Test info message'
+        }
+        It "should write a warning log" {
+            Write-WarningLog 'Test warning message'
+        }
+        It "should write a error log" {
+            Write-ErrorLog 'Test error message'
+        }
+        It "should write a fatal log" {
+            Write-FatalLog 'Test fatal message'
+        }
+        AfterAll {
+            Close-Logger
+        }
     }
-    It "should write a debug log without throwing"{
-        { Write-DebugLog 'Test Debug message' } | Should -Not -Throw
-    }
-    It "should write a info log without throwing"{
-        { Write-InfoLog 'Test info message' } | Should -Not -Throw
-    }
-    It "should write a warning log without throwing"{
-        { Write-WarningLog 'Test warning message' } | Should -Not -Throw
-    }
-    It "should write a error log without throwing"{
-        { Write-ErrorLog 'Test error message' } | Should -Not -Throw
-    }
-    It "should write a fatal log without throwing"{
-        { Write-FatalLog 'Test fatal message' } | Should -Not -Throw
-    }
-
-    It "should display formatted output without throwing"{
-        {
-            $position = @{
-                Latitude  = 25
-                Longitude = 134
-            }
-            $elapsedMs = 34
-            Write-InfoLog 'Processed {@Position} in {Elapsed:000} ms.' -PropertyValues $position, $elapsedMs
-        } | Should -Not -Throw
-    }
-    
-    It "should close logger without throwing"{
-        { Close-Logger } | Should -Not -Throw
-    }
-
-    AfterAll {
-        Remove-Item "$here\testlog.log"
-    }
-    
 }
+
+Set-Location $beforeChangeLocation
