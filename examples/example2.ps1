4e43e1cdb7fb26c1afdeeae404082eb32b34d9da
Tomáš Bouda <email@tomasbouda.cz>
2020-06-26 11:45:06 +0200

    example2

--- examples/example2.ps1
+++ examples/example2.ps1
@@ -6,16 +6,16 @@ Import-Module "$PSScriptRoot\..\src\PoShLog.psm1" -Force
 
 New-Logger |
 	Set-MinimumLevel -Value Verbose -ToPreference |
-	Add-EnrichWithErrorRecord |
-	Add-EnrichFromLogContext |
-	Add-SinkPowerShell -OutputTemplate '[{Timestamp:HH:mm:ss}] {Message:lj} {ErrorRecord}' |
+	Add-SinkFile -Path 'C:\Data\Log\test.log' |
+	Add-SinkPowerShell  |
 	Start-Logger
-	
 
 # Test all log levels
 Write-VerboseLog 'Test verbose message'
-Write-DebugLog 'Test debug message'
-Write-DebugLog 'Test debug message'
+'Test debug message' | Write-DebugLog
+Write-DebugLog 'Test debug message {Num}' -PropertyValues 321
+Write-DebugLog 'asd'
+Write-DebugLog -MessageTemplate 'Write-DebugLogasd'
 Write-InfoLog 'Test info message2'
 Write-InformationLog 'Test info message2'
 Write-WarningLog 'Test warning message'
@@ -25,17 +25,20 @@ Write-FatalLog 'Test fatal message'
 try {
     ConvertFrom-Json 'asd'
 } catch {
-    Write-ErrorLog 'Error occured' -ErrorRecord $_
+	# Write-ErrorLog -MessageTemplate 'asd' -ErrorRecord $_
+	# Write-ErrorLog 'asd {asd}' -ErrorRecord $_ -PropertyValues 321
+	# 'Test debug message' | Write-ErrorLog -ErrorRecord $_
+	Write-ErrorLog -ErrorRecord $_
 }
 
 
 # Example of formatted output
-$position = @{
-    Latitude = 25
-    Longitude = 134
-}
-$elapsedMs = 34
+# $position = @{
+#     Latitude = 25
+#     Longitude = 134
+# }
+# $elapsedMs = 34
 
-Write-InfoLog 'Processed {@Position} in {Elapsed:000} ms.' -PropertyValues $position, $elapsedMs
+# Write-InfoLog 'Processed {@Position} in {Elapsed:000} ms.' -PropertyValues $position, $elapsedMs
 
 Close-Logger
