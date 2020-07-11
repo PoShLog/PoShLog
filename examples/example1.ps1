b5928497a1edbcee185abd537244e728a438494a
Tomáš Bouda <email@tomasbouda.cz>
2020-07-11 16:16:25 +0200

    examples

--- examples/example1.ps1
+++ examples/example1.ps1
@@ -1,66 +1,21 @@
-Import-Module "C:\Data\GIT\PoShLog\PoShLog.Core\src\PoShLog.psm1" -Force
-Import-Module 'C:\Data\GIT\PoShLog\PoShLog.Core\publish\PoShLog.Enrichers'
-Import-Module 'C:\Data\GIT\PoShLog\PoShLog.Core\publish\PoShLog.Sinks.Exceptionless'
-
-# Get-Command Add-SinkFile -Syntax
-# exit
-# Start-Logger -Console
-
-# Write-InfoLog 'Hurrray, my first log message'
-
-# exit
-
-# # Create and start new logger
-# Start-Logger -MinimumLevel Verbose -FilePath 'C:\Data\my_awesome-.log' -FileRollingInterval Day
-
-# Write-InfoLog 'Hurrray, my first log message'
-# exit
-
-# $position = @{ 
-# 	Latitude = 25
-# 	Longitude = 134 
-# }
-# $elapsedMs = 34
-
-# Write-InfoLog 'Processed {@Position} in {Elapsed:000} ms.' -PropertyValues $position, $elapsedMs
-
-
-# Close-Logger
-# exit
+Import-Module PoShLog
+Import-Module PoShLog.Enrichers
 
 # Level switch allows you to switch minimum logging level
 $levelSwitch = New-LevelSwitch -MinimumLevel Verbose
 
-# $DebugPreference = "Continue"
-# $VerbosePreference = "Continue"
-
-# Test default methods if Logger is not available
-# Write-VerboseLog "test verbose"
-# Write-DebugLog -MessageTemplate "test debug asd"
-# Write-InfoLog -MessageTemplate  "test info {asd}, {num}" -PropertyValues "test1", 321
-# Write-WarningLog "test warning"
-# Write-ErrorLog -MessageTemplate "test error {asd}, {num}" -PropertyValues "test2", 123
-# Write-FatalLog "test fatal"
-
 $l0 = New-Logger |
 	Set-MinimumLevel -Value Verbose -ToPreference |
-	# Add-EnrichWithProperty -Name 'test' -Value 'aaa' |
-	# Add-EnrichWithEnvironment |
-	# Add-EnrichWithExceptionDetails |
-	# Add-EnrichFromLogContext |
+	Add-EnrichWithEnvironment |
+	Add-EnrichWithExceptionDetails |
+	Add-EnrichFromLogContext |
 	Add-SinkPowerShell |
 	Add-SinkConsole -OutputTemplate "[{MachineName} {Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}" | 
 	Start-Logger -PassThru -SetAsDefault
 
-# $LoggerConfig = New-Logger
-# $LoggerConfig = [Serilog.SomeNamespace]::PSConsole($LoggerConfig.WriteTo)
-# $LoggerConfig | Start-Logger
-
-Write-InfoLog -MessageTemplate 'some default log'
+Write-InfoLog -MessageTemplate 'Some default log'
 
 Close-Logger -Logger $l0
-
-$l0.Information("blballb")
 
 # Setup new logger
 $logger1 = New-Logger |
@@ -68,8 +23,7 @@ $logger1 = New-Logger |
 	Add-EnrichWithEnvironment |
 	Add-EnrichWithExceptionDetails |
 	Add-SinkFile -Path "C:\Logs\test-.txt" -RollingInterval Hour -OutputTemplate '{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception} {Properties:j}{NewLine}' |
-	# Add-SinkConsole -OutputTemplate "[{MachineName} {Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}" | 
-	Add-SinkPowerShell |
+	Add-SinkConsole -OutputTemplate "[{MachineName} {Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}" | 
 	Start-Logger -PassThru
 
 $logger2 = Start-Logger -FilePath "C:\Logs\test2-.txt" -Console -PassThru -MinimumLevel Verbose
@@ -81,7 +35,6 @@ Set-Logger -Logger $logger2
 Write-InfoLog -MessageTemplate $null
 Write-InfoLog -MessageTemplate ''
 Write-InfoLog -MessageTemplate 'asd {0} - {1}' -PropertyValues $null, '' -Exception $null
-
 
 Write-DebugLog -MessageTemplate "test debug asdasdsad"
 
@@ -97,7 +50,7 @@ try {
 	Get-Content -Path 'asd' -ErrorAction Stop
 }
 catch {
-	Write-FatalLog -Exception $_.Exception -MessageTemplate 'Error reading file!'
+	Write-FatalLog -ErrorRecord $_ -MessageTemplate 'Error while reading file!'
 }
 
 Close-Logger
--- examples/example2.ps1
+++ examples/example2.ps1
@@ -1,23 +1,19 @@
-# $VerbosePreference = 'SilentlyContinue'
-# $DebugPreference = 'Continue'
-# $InformationPreference = 'Continue'
-
-Import-Module "$PSScriptRoot\..\src\PoShLog.psm1" -Force
+Import-Module PoShLog
 
 New-Logger |
 	Set-MinimumLevel -Value Verbose -ToPreference |
-	Add-SinkFile -Path 'C:\Data\Log\test.log' |
-	Add-SinkPowerShell  |
+	Add-EnrichWithErrorRecord |
+	Add-SinkFile -Path 'C:\Logs\test.log' |
+	Add-SinkPowerShell |
 	Start-Logger
 
 # Test all log levels
 Write-VerboseLog 'Test verbose message'
-'Test debug message' | Write-DebugLog
+'Test debug message usig pipeline' | Write-DebugLog
 Write-DebugLog 'Test debug message {Num}' -PropertyValues 321
-Write-DebugLog 'asd'
-Write-DebugLog -MessageTemplate 'Write-DebugLogasd'
-Write-InfoLog 'Test info message2'
-Write-InformationLog 'Test info message2'
+Write-DebugLog -MessageTemplate 'Write-DebugLog using MessageTemplate'
+Write-InfoLog 'Test info message'
+Write-InformationLog 'Test info message 2'
 Write-WarningLog 'Test warning message'
 Write-ErrorLog 'Test error message'
 Write-FatalLog 'Test fatal message'
@@ -25,20 +21,7 @@ Write-FatalLog 'Test fatal message'
 try {
     ConvertFrom-Json 'asd'
 } catch {
-	# Write-ErrorLog -MessageTemplate 'asd' -ErrorRecord $_
-	# Write-ErrorLog 'asd {asd}' -ErrorRecord $_ -PropertyValues 321
-	# 'Test debug message' | Write-ErrorLog -ErrorRecord $_
-	Write-ErrorLog -ErrorRecord $_
+	Write-ErrorLog 'Error while converting fron json!' -ErrorRecord $_
 }
-
-
-# Example of formatted output
-# $position = @{
-#     Latitude = 25
-#     Longitude = 134
-# }
-# $elapsedMs = 34
-
-# Write-InfoLog 'Processed {@Position} in {Elapsed:000} ms.' -PropertyValues $position, $elapsedMs
 
 Close-Logger
